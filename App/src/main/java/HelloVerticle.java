b5f7dec58462dd1b0a37a4880197c2e415ddb854
Blessonpx <blessonxavierofficial@gmail.com>
2025-03-10 19:47:50 +0530

    Working ch2 listing 2.1

---
+++ App/src/main/java/HelloVerticle.java
@@ -0,0 +1,41 @@
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+public class HelloVerticle extends AbstractVerticle{
+	private final Logger logger = LoggerFactory.getLogger(HelloVerticle.class);
+	private long counter=1;
+	
+	@Override
+	public void start() {
+		/*
+		 * This defines a periodic 
+		 * task every five seconds.
+		 * 
+		 * */
+		vertx.setPeriodic(5000,id->{
+			logger.info("tick");
+		});
+		
+		/*
+		 * The HTTP server calls this
+		 * handler on every request.
+		 * 
+		 * */
+		vertx.createHttpServer().requestHandler(req->{
+			logger.info("Request #{} from {}",counter++,
+					req.remoteAddress().host());
+			req.response().end("Hello!");
+		})
+		.listen(8080);
+		logger.info("Open http://localhost:8080/");
+		
+	}
+	
+	public static void main(String[] args) {
+		Vertx vertx = Vertx.vertx(); // We need a global Vert.x instance.
+		vertx.deployVerticle(new HelloVerticle());  // This is the simplest way to deploy a verticle.
+	}
+}
