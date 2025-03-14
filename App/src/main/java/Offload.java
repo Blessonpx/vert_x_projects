044eb008a04a12e6b006ef742f9e121432f7b1f3
Blessonpx <blessonxavierofficial@gmail.com>
2025-03-14 14:07:59 +0530

    ExecutingBlock 2

--- App/src/main/java/Offload.java
+++ App/src/main/java/Offload.java
@@ -14,21 +14,36 @@ public class Offload extends AbstractVerticle{
 	public void start() {
 		vertx.setPeriodic(5000,id->{
 			logger.info("Tick");
+			/*
+			 * executeBlocking takes two parameters: the code to run and a callback for when it has run.
+			 * */
 			vertx.executeBlocking(this::blockingCode,this::resultHandler);
 		});
 	}
 	
 	private void blockingCode(Promise<String> promise) {
+		/*
+		 * The blocking code takes a Promise object of any type. 
+		 * It is used to eventually pass the result.
+		 * */
 		try {
 			logger.info("Executing Blocking code ...");
 			Thread.sleep(4000);
 			logger.info("Done!");
 			promise.complete("OK!");
+			/*
+			 * The Promise object needs to either complete or fail, 
+			 * marking the end of the blocking code execution.
+			 * */
 		}catch(InterruptedException e ) {
 			promise.fail(e);
 		}
 	}
 	
+	/*
+	 * Processing the result on the event loop is just another asynchronous result.
+	 * 
+	 * */
 	private void resultHandler(AsyncResult<String> ar) {
 		if(ar.succeeded()) {
 			logger.info("Result of Blocking code {}",ar.result());
