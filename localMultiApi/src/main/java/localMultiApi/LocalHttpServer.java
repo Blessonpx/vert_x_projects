bd566918030025a3c1985bde0afdf82103f19b7f
Blessonpx <blessonxavierofficial@gmail.com>
2025-03-19 20:47:41 +0530

    Commit of Web API test chap_3

---
+++ localMultiApi/src/main/java/localMultiApi/LocalHttpServer.java
@@ -0,0 +1,41 @@
+package localMultiApi;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+public class LocalHttpServer extends AbstractVerticle{
+	private static final Logger logger = LogManager.getLogger(LocalHttpServer.class);
+	
+	/*
+	 * Http serrver
+	 * does multi routing creating 2 apis 
+	 * */
+	@Override
+	public void start() {
+		Router router = Router.router(vertx);
+		
+		router.get("/").handler(ctx->{
+			System.out.println("Serving '/' endpoint ");
+			ctx.response().send("Hi");
+		});
+		
+		router.get("/api1/:param").handler(this::handleApi1);
+		
+		vertx.createHttpServer()
+		.requestHandler(router)
+		.listen(8080);
+	}
+	
+	private void handleApi1(RoutingContext ctx) {
+		String param= ctx.pathParam("param");
+		System.out.println("Recieved Param"+param);
+		logger.info("Recieved Param"+param);
+		ctx.response().end("You have sent param: "+param);
+	}
+	
+	
+}
