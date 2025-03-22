0e72b861defbc831ed6112f1e687366d5dc33079
Blessonpx <blessonxavierofficial@gmail.com>
2025-03-22 11:27:49 +0530

    Chapter 4 part 2

---
+++ Ch4_Listings/src/main/java/ReadViaVertx.java
@@ -0,0 +1,34 @@
+import io.vertx.core.Vertx;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.OpenOptions;
+
+public class ReadViaVertx {
+	public static void main(String[] args) {
+		Vertx vertx = Vertx.vertx();
+		OpenOptions opts = new OpenOptions().setRead(true);
+		vertx.fileSystem().open("/home/blesson/code/java_files/vert_x_projects/Ch4_Listings/Abc.txt",
+				opts, ar->{
+					if(ar.succeeded()) {
+						/*
+						 * AsyncFile is the interface for Vert.x asynchronous files
+						 * */
+						AsyncFile file = ar.result(); 
+						/*
+						 * Below Handling is declarative, declaring beforehand 
+						 * We are pushed the contents of the file unlike being pulled in case of ReadFileJDK
+						 * 
+						 * */
+						file.handler(System.out::println)
+							.exceptionHandler(Throwable::printStackTrace)
+							.endHandler(done->{
+								System.out.println("\n ---------- Done");
+								vertx.close();
+							});
+							
+					}else {
+						ar.cause().printStackTrace();
+					}
+				});
+	}
+
+}
