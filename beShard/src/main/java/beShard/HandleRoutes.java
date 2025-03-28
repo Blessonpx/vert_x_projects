26010c34df8ce4272b4db4b954b2ffa71c6c4a1a
Blessonpx <blessonxavierofficial@gmail.com>
2025-03-28 16:31:33 +0530

    First run- be Shard

--- beShard/src/main/java/beShard/HandleRoutes.java
+++ beShard/src/main/java/beShard/HandleRoutes.java
@@ -1,12 +1,14 @@
 package beShard;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.client.HttpResponse;
 import io.vertx.ext.web.client.WebClient;
@@ -15,57 +17,46 @@ import io.vertx.ext.web.handler.BodyHandler;
 public class HandleRoutes extends AbstractVerticle {
 	private static final Logger logger =  LoggerFactory.getLogger(HandleRoutes.class);
 	
-	private Router router;
-	private WebClient webClient;
 	/*
 	 * Declare Handlers
 	 * 
 	 * */
 	
 	@Override
-	public void start() {
+	public void start() {	
+		logger.info("HTTP Server started on port");
+		WebClient client = WebClient.create(vertx);
+		Map<String,String> data = new HashMap<>();
+		data.put("name", "Alice");
+		data.put("occupation", "Engineer");
 		
-		router = Router.router(vertx);
-		webClient = WebClient.create(vertx);
-
-		vertx.createHttpServer()
-		.requestHandler(router)
-		.listen(8080);
-		
-		BodyHandler bodyHandler = BodyHandler.create();
-		
-		router.post().handler(bodyHandler);
-		router.get().handler(bodyHandler);
-		
-		router.post("/postApi").handler(this::register);
-	}
-	
-	
-	private void sendStatusCode(RoutingContext ctx, int code) {
-		ctx.response().setStatusCode(code).end();
-	}
-	
-	private void sendBadGateWay(RoutingContext ctx,Throwable err) {
-		logger.error("Whoops",err);
-		ctx.fail(502);
-	}
-	
-	private void register(RoutingContext ctx) {
-		webClient
-		.post(3000, "localhost","/postApi")
-		.putHeader("Content-Type", "application/json")
-		.sendJsonObject(ctx.body().asJsonObject(),ar->{
-			if(ar.succeeded()) {
-				HttpResponse<Buffer> response = ar.result();
-				sendStatusCode(ctx,200);
-			}else {
-				sendBadGateWay(ctx,ar.cause());
-			}
-		});
 		/*
-		 * This converts the request from a Vert.x 
-		 * Buffer to a JsonObject.
+		 * First Start the HttpServer
 		 * 
 		 * */
+		
+		vertx.createHttpServer()
+			.requestHandler(req->{
+				req.response().end("Hello from /postApi");
+			})
+			.listen(8080,http->{
+				if(http.succeeded()) {
+					logger.info("Http Server started on Port ");
+					
+					
+					client.post(8080,"localhost","/postApi").sendJson(data,response->{
+						if(response.succeeded()) {
+							HttpResponse<Buffer> httpResponse = response.result();
+							logger.info("Sending response {}",httpResponse.statusCode());
+							logger.info("Sending json{}",data.toString());
+						}else {
+							logger.error(response.cause().getMessage());
+						}
+					});
+				}else {
+					logger.error("Failed to start HTTP server",http.cause());
+				}
+			});
+		
 	}
 }
