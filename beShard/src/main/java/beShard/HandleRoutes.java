0e62746a6f3594c50366194c800602f03c46da0d
Blessonpx <blessonxavierofficial@gmail.com>
2025-04-01 17:55:18 +0530

    BeShard working Apr 1

--- beShard/src/main/java/beShard/HandleRoutes.java
+++ beShard/src/main/java/beShard/HandleRoutes.java
@@ -1,18 +1,11 @@
 package beShard;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.buffer.Buffer;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.client.HttpResponse;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.Router;
 
 public class HandleRoutes extends AbstractVerticle {
 	private static final Logger logger =  LoggerFactory.getLogger(HandleRoutes.class);
@@ -23,40 +16,14 @@ public class HandleRoutes extends AbstractVerticle {
 	 * */
 	
 	@Override
-	public void start() {	
+	public void start() {
+		Router router = Router.router(vertx);
 		logger.info("HTTP Server started on port");
-		WebClient client = WebClient.create(vertx);
-		Map<String,String> data = new HashMap<>();
-		data.put("name", "Alice");
-		data.put("occupation", "Engineer");
-		
-		/*
-		 * First Start the HttpServer
-		 * 
-		 * */
+		router.get("/").handler(ctx->{
+			ctx.response().send("you passed ");
+		});
 		
 		vertx.createHttpServer()
-			.requestHandler(req->{
-				req.response().end("Hello from /postApi");
-			})
-			.listen(8080,http->{
-				if(http.succeeded()) {
-					logger.info("Http Server started on Port ");
-					
-					
-					client.post(8080,"localhost","/postApi").sendJson(data,response->{
-						if(response.succeeded()) {
-							HttpResponse<Buffer> httpResponse = response.result();
-							logger.info("Sending response {}",httpResponse.statusCode());
-							logger.info("Sending json{}",data.toString());
-						}else {
-							logger.error(response.cause().getMessage());
-						}
-					});
-				}else {
-					logger.error("Failed to start HTTP server",http.cause());
-				}
-			});
-		
+		.requestHandler(router).listen(8080);
 	}
 }
